14ccf7192eb8c4d2270bc5d0d13e824da35f2281
Muntasir <bvllr5@inf.elte.hu>
2024-03-10 00:46:34 +0100

    Navbar Updated

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,24 @@ import "./navbar.scss";
 const Navbar = () => {
     return (
         <div className="navbar">
-        <h1>Navbar</h1>
+            <div className="logo">
+                <img src="logo.svg" alt="" />
+                <span>ADMIN TEMPLATE</span>
+            </div>
+            <div className="icons">
+                <img src="/search.svg" alt="" className="icon" />
+                <img src="/app.svg" alt="" className="icon" />
+                <img src="/expand.svg" alt="" className="icon" />
+                <div className="notifications">
+                    <img src="/notifications.svg" alt=""/>
+                    <span>1</span>
+                </div>
+                <div className="user">
+                    <img src="https://www.svgrepo.com/show/9695/avatar.svg" alt=""/>
+                    <span>Jane</span>
+                </div>
+                <img src="/settings.svg" alt="" className="icon" />
+            </div>
         </div>
     );
 }
