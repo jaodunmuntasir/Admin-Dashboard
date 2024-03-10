624c2da658fc39f940b1f9539f9f6923e5529cc5
Muntasir <bvllr5@inf.elte.hu>
2024-03-10 12:31:35 +0100

    Menu Bar Added and Updated

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="footer">
         <span>Admin Template</span>
-        <span>@ 2024 All rights reserved | <a href="https://github.com/jaodunmuntasir">Jaodun Muntasir</a></span>
+        <span>@2024 All rights reserved | <a href="https://github.com/jaodunmuntasir">Jaodun Muntasir</a></span>
         </div>
     );
 }
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,9 +1,42 @@
+import { Link } from "react-router-dom";
 import "./menu.scss";
+import {menu} from "../../data.ts";
   
 const Menu = () => {
     return (
         <div className="menu">
-        <h1>Menu</h1>
+            {menu.map((item) => (
+                <div className="item" key={item.id}>
+                    <span className="title">{item.title}</span>
+                    {item.listItems.map((listItem) => (
+                        <Link to={listItem.url} className="listItem" key={listItem.id}>
+                            <img src={listItem.icon} alt={listItem.title} />
+                            <span className="listItemTitle">{listItem.title}</span>
+                        </Link>
+                    ))}
+                </div>
+            ))}
+
+            {/* <div className="item">
+                <span className="title">LISTS</span>
+                <Link to="/users" className="listItem">
+                    <img src="/user.svg" alt="users" />
+                    <span className="listItemTitle">Users</span>
+                </Link>
+                <Link to="/products" className="listItem">
+                    <img src="/product.svg" alt="products" />
+                    <span className="listItemTitle">Products</span>
+                </Link>
+                <Link to="/orders" className="listItem">
+                    <img src="/order.svg" alt="orders" />
+                    <span className="listItemTitle">Orders</span>
+                </Link>
+                <Link to="/posts" className="listItem">
+                    <img src="/post2.svg" alt="posts" />
+                    <span className="listItemTitle">Posts</span>
+                </Link>
+            </div> */}
+
         </div>
     );
 }
